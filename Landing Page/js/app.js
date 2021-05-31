d3155f0b93815dc04bce43935cd65fd7f3f15091
GuptaSanskriti <gupta.sanskriti05@gmail.com>
2021-06-01 02:06:22 +0530

    js file changes

--- Landing Page/js/app.js
+++ Landing Page/js/app.js
@@ -1,4 +1,25 @@
-import data from "./data.js";
+const data = [
+  {
+     id: 1,
+     name: "section-1",
+     info: "#section1"
+  },
+  {
+      id: 2,
+      name: "section-2",
+      info: "#section2"
+   },
+   {
+      id: 3,
+      name: "section-3",
+      info: "#section3"
+   },
+   {
+      id: 4,
+      name: "section-4",
+      info: "#section4"
+   }
+  ];
 
 const navMenu = document.querySelector('.navbar__menu'); 
 const classes = ['section1', 'section2', 'section3', 'section4'];
